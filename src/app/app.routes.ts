f392aea14dd974f537920a89bddbe4e45233aed5
M0stafaKareem <93432503+M0stafaKareem@users.noreply.github.com>
2024-08-04 01:52:58 +0300

    adding cart component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { ProductsPageComponent } from './products/all-products/products-page/products-page.component';
 import { SingleProductPageComponent } from './products/product-details/single-product-page/single-product-page.component';
 import { AuthenticationPageComponent } from './authentication-page/authentication-page.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
 
 export const routes: Routes = [
   {
@@ -20,5 +21,9 @@ export const routes: Routes = [
   {
     path: 'register',
     component: AuthenticationPageComponent,
+  },
+  {
+    path: 'cart',
+    component: CartPageComponent,
   },
 ];
--- src/app/authentication-page/authentication-page.component.ts
+++ src/app/authentication-page/authentication-page.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-authentication-page',
   standalone: true,
   imports: [],
   templateUrl: './authentication-page.component.html',
-  styleUrl: './authentication-page.component.css'
+  styleUrl: './authentication-page.component.css',
 })
 export class AuthenticationPageComponent {
-
+  isRegistered: boolean = false;
+  constructor(private router: Router) {
+    this.isRegistered = this.router.url == '/login' ? true : false;
+    console.log(this.isRegistered);
+  }
 }
