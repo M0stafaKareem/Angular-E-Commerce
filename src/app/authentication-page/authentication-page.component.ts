4747d83c008989911802ea86ce8528b4bc71d6e1
M0stafaKareem <93432503+M0stafaKareem@users.noreply.github.com>
2024-08-04 13:54:37 +0300

    controlling authentication forms

--- src/app/authentication-page/authentication-page.component.ts
+++ src/app/authentication-page/authentication-page.component.ts
@@ -1,17 +1,43 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-authentication-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './authentication-page.component.html',
   styleUrl: './authentication-page.component.css',
 })
 export class AuthenticationPageComponent {
   isRegistered: boolean = false;
+
   constructor(private router: Router) {
     this.isRegistered = this.router.url == '/login' ? true : false;
-    console.log(this.isRegistered);
+  }
+
+  // reactive Sign up form
+  fullNameInput = new FormControl('');
+  emailInput = new FormControl('');
+  telInput = new FormControl('');
+  passwordInput = new FormControl('');
+  signUpClickHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.fullNameInput.value);
+    console.log(this.telInput.value);
+    console.log(this.emailInput.value);
+    console.log(this.passwordInput.value);
+    this.fullNameInput.reset();
+    this.telInput.reset();
+    this.emailInput.reset();
+    this.passwordInput.reset();
+  }
+
+  // template driven Login form
+  loginEmail: string = '';
+  loginPassword: string = '';
+  loginClickHandler() {
+    console.log(this.loginEmail);
+    console.log(this.loginPassword);
   }
 }
